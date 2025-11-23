2cbabc335d4a0de40f237fecca0e76423f3b3875
AlejandroMagana95 <35556365+AlejandroMagana95@users.noreply.github.com>
2025-11-23 14:56:54 -0700

    Add Node class for binary tree representation

--- src/main/java/com/mycompany/app/Node.java
+++ src/main/java/com/mycompany/app/Node.java
@@ -1,1 +1,24 @@
+class Node extends BT {
+
+    int num;
+    BT left;
+    BT right;
+
+    Node(int n, BT l, BT r) {
+        num = n;
+        left = l;
+        right = r;
+    }
+
+    public int height() {
+        int lh = left.height();
+        int rh = right.height();
+        return (lh < rh ? rh : lh) + 1;
+    }
+
+    @Override
+    public String toString() {
+        return "Node(" + num + ", " + left.toString() + ", " + right.toString() + ")";
+    }
+}
 
