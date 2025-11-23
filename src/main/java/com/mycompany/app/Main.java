1bb38ea00b1b5d8764dd62fd0e19c0c4aae513db
AlejandroMagana95 <35556365+AlejandroMagana95@users.noreply.github.com>
2025-11-23 14:57:12 -0700

    Add main class with binary tree example

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -1,1 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
 
+        BT nil = Nil.getNil();
+
+        BT tree = new Node(
+                3,
+                new Node(1, nil, new Node(4, nil, nil)),
+                new Node(2, nil, nil)
+        );
+
+        System.out.println(tree);
+    }
+}
