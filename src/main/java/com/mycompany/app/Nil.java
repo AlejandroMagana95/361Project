94e285fd6fc8547b104dd276ed41e39bb3d10415
AlejandroMagana95 <35556365+AlejandroMagana95@users.noreply.github.com>
2025-11-23 14:56:32 -0700

    Implement Singleton pattern for Nil class

--- src/main/java/com/mycompany/app/Nil.java
+++ src/main/java/com/mycompany/app/Nil.java
@@ -1,1 +1,20 @@
+class Nil extends BT {
 
+    private static Nil nil;
+
+    private Nil() {}
+
+    public static Nil getNil() {
+        if (nil == null)
+            nil = new Nil();
+        return nil;
+    }
+
+    public int height() {
+        return -1;
+    }
+
+    public String toString() {
+        return "Nil";
+    }
+}
