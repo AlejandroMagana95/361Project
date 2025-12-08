c4cc075ff456e11d4836ace62377533dacac2a2d
AlejandroMagana95 <35556365+AlejandroMagana95@users.noreply.github.com>
2025-12-07 19:16:09 -0700

    Update Project2.java

--- src/main/java/com/mycompany/app/Project2.java
+++ src/main/java/com/mycompany/app/Project2.java
@@ -1,1 +1,24 @@
+public class Item {
+    int id;
+    double weight;
+    double value;
+    double priority;
+
+    // Constructor
+    public Item(int id, double weight, double value, double priority) {
+        this.id = id;
+        this.weight = weight;
+        this.value = value;
+        this.priority = priority;
+    }
+
+    // Optional but very useful for printing/debugging
+    @Override
+    public String toString() {
+        return "Item " + id +
+               " | weight=" + weight +
+               " | value=" + value +
+               " | priority=" + priority;
+    }
+}
 
